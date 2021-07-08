d673de7cee0829bf1a46bee5e681d8467fcb808e
yingstm124 <yingstm124@gmail.com>
2021-07-08 23:14:29 +0700

    Segment Student, Score, row cell

--- ImageProcessing.py
+++ ImageProcessing.py
@@ -2,40 +2,31 @@ import cv2
 import numpy as np
 
 import Utility
+import Processing
 
 
 class BinaryImage:
 
     def __init__(self, image, debug=False):
         self.debug = debug
+        self.rgb_image = image
 
-        gray_image =image = self.convert2grayImage(image)
-        remove_shadow_image = self.removeShadow(gray_image)
-
-        self.binary_image = self.convert2binaryImage(remove_shadow_image)
-
-
-    def convert2grayImage(self, rgb_img):
-        gray = cv2.cvtColor(rgb_img, cv2.COLOR_BGR2GRAY)
+    def getBinaryImage(self):
+        # 1. convert gray image
+        gray_img = cv2.cvtColor(self.rgb_image, cv2.COLOR_BGR2GRAY)
         if (self.debug):
-            Utility.showImage(gray,'gray image')
-    
-        return gray
-
-    def removeShadow(self, gray_img):
+            Utility.showImage(gray_img,'gray image')
+        
+        # 2. remove shadow
         dilated_img = cv2.dilate(gray_img, np.ones((7,7), np.uint8))
         bg_img = cv2.medianBlur(dilated_img, 99)
         diff_img = 255 - cv2.absdiff(gray_img, bg_img)
         norm_img = diff_img.copy()
         cv2.normalize(diff_img, norm_img, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8UC1)
-
         if(self.debug):
             Utility.showImage(diff_img, 'remove shadow')
 
-        return diff_img
-
-    def convert2binaryImage(self, gray_img):
-
+        # 3. convert 1-channel image (binary image)
         bilateral = cv2.bilateralFilter(gray_img, 11, 40, 40)
         blur = cv2.GaussianBlur(bilateral,(5,5),0)
         thresh_img = cv2.adaptiveThreshold(blur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 35, 10)
@@ -44,22 +35,33 @@ class BinaryImage:
             # Utility.showImage(bilateral, 'bilateral image')
             # Utility.showImage(blur, 'blur')
             Utility.showImage(thresh_img, 'thresholding (binary image)')
-            
-        return thresh_img
+        
+        return  thresh_img
 
 
 class CellSheets:
 
     def __init__(self, binaryImage, debug=False, max_cols=20):
         self.debug = debug
+        self.binary_image = binaryImage  
+        self.max_cols = max_cols    
+        self.amountOfRow = 0
 
-        contour_sheet = self.contours(binaryImage, 'RETR_LIST')
-        sheet_width = binaryImage.shape[1]
-        cell_minWidth = (sheet_width/max_cols)
-        cell_bounding = []
+    def getCellSheets(self):
 
-        contour_sheet_sorted, _ = self.sortContours(contour_sheet)
+        # 1. contour all for finding cell
+        contour_sheet = Processing.contours(self.binary_image, 'RETR_LIST')
 
+        # 2. sorting contour
+        contour_sheet_sorted, _ = Processing.sortContours(contour_sheet, method="left-to-right", debug=self.debug)
+        
+        # 3. add cell image in cell bounding array
+        sheet_width = self.binary_image.shape[1]
+        cell_minWidth = (sheet_width/self.max_cols)
+        row_cell = []
+        digit_cell = []
+        student_id_cell = []
+        temp_cell = []
         i = 0
         amount_of_row = 0
         first_row_width = 0
@@ -70,50 +72,42 @@ class CellSheets:
             if (len(approx) == 4):
                 x, y, w, h = cv2.boundingRect(approx)
 
-                if(w > cell_minWidth):
-                    print("(width,height) : ({0},{1})".format(w,h))
-                    cell_bounding.append(binaryImage[y:y+h, x:x+w])
+                is_digit_cell = True if w/h < 2 else False
 
-                    if(i == 1):
+                if(w > cell_minWidth):
+
+                    temp_cell.append(self.binary_image[y:y+h, x:x+w])
+
+                    if(is_digit_cell):
+                        digit_cell.append(self.binary_image[y:y+h, x:x+w])
+                        if(self.debug):
+                            Utility.showImage(self.binary_image[y:y+h, x:x+w],"digit cell (width,height) : ({0},{1})".format(w,h))
+
+                    if(i == 1): 
                         first_row_width = w
                     
                     if(w >= first_row_width-100 and w <= first_row_width+100 and first_row_width > 0):
                         amount_of_row = amount_of_row + 1
+                        row_cell.append(self.binary_image[y:y+h, x:x+w])
         
-                    if(self.debug):
-                        Utility.showImage(binaryImage[y:y+h, x:x+w],"segment")
+                        if(self.debug):
+                            Utility.showImage(self.binary_image[y:y+h, x:x+w],"row cell (width,height) : ({0},{1})".format(w,h))
                     
                     i = i+1
-                    
-        self.scoreSheet = cell_bounding
+
         self.amountOfRow = amount_of_row
-
-    def contours(self, binary_image, mode="RETR_EXTERNAL"):
-        if mode == "RETR_EXTERNAL":
-            contours, _ = cv2.findContours(binary_image , cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        elif mode == "RETR_LIST":
-            contours, _ = cv2.findContours(binary_image , cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+        student_id_cell = temp_cell[amount_of_row+1]
+        if(self.debug):
+            Utility.showImage(student_id_cell,"student id cell (width,height) : ({0},{1})".format(w,h))
         
-        return contours
+        return row_cell, student_id_cell, score_cell
 
-    def sortContours(self,contours, method="left-to-right"):
-        reverse = False
-        i = 0
-        if method == "right-to-left" or method == "bottom-to-top":
-            reverse = True
-            
-        if method == "top-to-bottom" or method == "bottom-to-top":
-            i = 1
-        
-        boundingBoxes = [cv2.boundingRect(c) for c in contours]
-        (cnts, boundingBoxes) = zip(*sorted(zip(contours, boundingBoxes),
-        key=lambda b:b[1][i], reverse=reverse))
-            
-        return (cnts, boundingBoxes)
 
-        
-# class DigitBox:
-#     def __init__(self, cellSheet, debug=False):
+class DigitBox:
+    def __init__(self, cellSheet, debug=False):
+        self.debug = debug
+        self.cell_sheet = cellSheet
+
 
 
 
--- Main.py
+++ Main.py
@@ -10,14 +10,21 @@ import cv2
 
 if __name__ == '__main__':
     
+    # 1. import original rgb image 
     img = cv2.imread('./asset/sample01.png')
     # Utility.showImage(img, 'original image')
 
-    binary_image = BinaryImage(img).binary_image
-    cell_sheets = CellSheets(binary_image, True)
-    cellBoxs = cell_sheets.scoreSheet
-    amount_of_row = cell_sheets.amountOfRow
-    print(amount_of_row) 
+    # 2. convert to binary image
+    binary_image = BinaryImage(img).getBinaryImage()
+
+    # 3. processing cell
+    row_cell, student_id_cell, score_cell = CellSheets(binary_image, False).getCellSheets()
+
+
+    # cellBoxs = cell_sheets.scoreSheet
+    # amount_of_row = cell_sheets.amountOfRow
+    # print("amount of row : ",amount_of_row)
+    # print(cellBoxs) 
     
     
 
